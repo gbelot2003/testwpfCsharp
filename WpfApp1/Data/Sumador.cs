736108064b9d9f40ce300c838a7e6be8bb1015e8
Gerardo Belot <gbelot2003@hotmail.com>
2017-12-29 16:11:00 -0600

    Primer Commit

---
+++ WpfApp1/Data/Sumador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Data
+{
+    public class Sumador
+    {
+
+        int sumando;
+        int sumador;
+
+        public Sumador(int sumando, int sumador)
+        {
+            this.sumando = sumando;
+            this.sumador = sumador;
+        }
+            
+        public int proceso()
+        {
+            return (this.sumando + this.sumador);
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using WpfApp1.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculo_Click(object sender, RoutedEventArgs e)
+        {
+            Sumador sumador = new Sumador(Convert.ToInt32(this.sumando.Text), Convert.ToInt32(this.sumador.Text));
+            this.resultados.Content = sumador.proceso().ToString();
+        }
+    }
+}
